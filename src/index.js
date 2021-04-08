226490591660b511bb02dd019c2cb8fd8d32cd0f
BRvanHogen <brvanhogen@hotmail.nl>
2021-04-08 19:37:39 +0200

    Stap 1 en 2: mapje context gemaakt, LanguageContext.js aangemaakt en geexporteerd. Deze geimporteerd in App.js.
    Stap 3: LC gewikkeld om App in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {LanguageContext} from "./context/LanguageContext";
 
 ReactDOM.render(
   <React.StrictMode>
+      <LanguageContext>
     <App />
+      </LanguageContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
